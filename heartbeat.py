1a00938926da6417cda0c7ed047a6e3d1056abca
Adam Long <adam.long.tech.recruiting@gmail.com>
2020-09-11 14:11:53 -0600

    Changed important info statements to warnings and restricted logging level to stop the pulse messages

--- heartbeat.py
+++ heartbeat.py
@@ -20,8 +20,7 @@ db = trades_db.DB(j, bot_configuration.DATA_FOLDER + bot_configuration.DATABASE_
 
 def pulse():
 	try:
-		j.bootstrap()
-
+		pull_queued_trades()
 		is_open = b.is_open()
 		#is_open = True
 
@@ -32,15 +31,13 @@ def pulse():
 			return
 		elif is_open == False:
 			if s.get_market_open() == True:
-				pull_queued_trades()
-				logging.info('Market has closed.')
+				logging.warning('Market has closed.')
 				s.set_market_open(False)
 			return
 
 		if s.get_market_open() == False:
-			pull_queued_trades()
 			s.set_market_open(True)
-			logging.info('Market has opened')
+			logging.warning('Market has opened')
 
 		trades_manager.expire_trades(b, db)
 		trades_manager.handle_open_buy_orders(b, db)
@@ -48,7 +45,7 @@ def pulse():
 		trades_manager.handle_open_trades(b, db, s)
 		trades_manager.open_new_trades(b, db, s)
 	except requests.exceptions.ConnectionError as conn:
-		logging.info(f'Bad connection. {conn.message}')
+		logging.warning(f'Bad connection. {conn.message}')
 	except Exception as err:
 		logging.error('Exception occured during heartbeat:', exc_info=err)
 
@@ -63,6 +60,6 @@ def pull_queued_trades():
 
 		trade = db.create_new_long_trade(row[0], row[2], row[3], row[4], row[6])
 		j.create_trade_record(trade, row[5], row[7], row[8])
-		logging.info(f'Trade added to Queue: [{row[0]}, long, {row[2]}, {row[3]}, {row[4]}]')
+		logging.warning(f'Trade added to Queue: [{row[0]}, long, {row[2]}, {row[3]}, {row[4]}]')
 
 	j.reset_queued_trades(header_row)
